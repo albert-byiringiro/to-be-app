7202b58c73b7290d4a911ce4d342835e95ba07e2
Albert Byiringiro <byiringiroalbert20@gmail.com>
2024-07-28 09:18:18 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// import './App.css'
+
+function App() {
+
+  return (
+    <div className="">
+      <h1 className="text-4xl text-blue-400">Hello, world!</h1>
+    </div>
+  )
+}
+
+export default App
