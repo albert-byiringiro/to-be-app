ed91603a9ba64e18face2f76f79cae3b0a1c075b
Albert Byiringiro <byiringiroalbert20@gmail.com>
2024-07-28 16:24:23 +0200

    Todo: Add functionality to update todo item

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Tobe from './components/Tobe';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlusCircle } from '@fortawesome/free-solid-svg-icons';
@@ -9,7 +9,17 @@ function App() {
 
   const [todo, setTodo] = useState("");
 
-  const [tobes, setTobes] = useState([]);
+  const [tobes, setTobes] = useState(
+    () => {
+      const saveTobes = localStorage.getItem('tobes');
+      return saveTobes ? JSON.parse(saveTobes) : [];
+    }
+  );
+
+  useEffect(() => {
+    // Save tobes to localStorage whenever it changes
+    localStorage.setItem('tobes', JSON.stringify(tobes));
+  }, [tobes]);
 
   const handleInput = (e) => {
     setTodo(e.target.value)
@@ -41,6 +51,16 @@ function App() {
     setTobes(prevTobes => prevTobes.filter(tobe => tobe.id !== id))
   }
 
+  /*
+    const handleUpdate = (id) => {
+    setTobes(prevTobes => {
+      return prevTobes.map(be => be.id === id ? {...be, tobe})
+    })
+  }
+  
+  */ 
+
+
   return (
     <main className="container mx-auto p-4">
       <h1 className="my-6 font-bold text-center text-gray-800 text-4xl">todos</h1>
@@ -55,7 +75,7 @@ function App() {
             onChange={handleInput}
           />
           <button type="submit" className="absolute right-4">
-            <FontAwesomeIcon className="text-xl" icon={faPlusCircle} />
+            <FontAwesomeIcon className="text-xl text-green-700" icon={faPlusCircle} />
           </button>
         </label>
       </form>
